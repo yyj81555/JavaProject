ea0ab4ce321eec662c88f796647b55324a0aabf0
chujihyeong <cjh939@gmail>
2022-04-04 17:09:17 +0900

    (+) SignUp 기능추가
     - SignUp 기능추가

--- client/src/Component/SignUpPage.js
+++ client/src/Component/SignUpPage.js
@@ -2,9 +2,14 @@ import React from 'react';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
-import { Box } from '@mui/system';
+import { Box,TextField } from '@mui/material';
+import { Button } from '@mui/material';
 
 export default function SignUpPage(props) {
+    const [inputName, setInputName] = React.useState("");
+    const [InputUserID, setInputUserID] = React.useState("");
+    const [InputUserPassword, setInputUserPassword] = React.useState("");
+    
     const styles = {
         dimmed_layer_wrapper : {
             position: "fixed",
@@ -15,6 +20,17 @@ export default function SignUpPage(props) {
         },
     };
 
+    const OnClickPostSignUp = async () => {
+        const response = await axios.post("/api/getPostSignUp", {
+          name: inputName,
+          userID: InputUserID,
+          userPassword: InputUserPassword,
+          contentType: "application/json; UTF-8;", // 한국어도 깨짐없이 전송하는 방법.
+        });
+        const body = response.data;
+    
+        //setName(body.data);
+      }
     const navigate = useNavigate();
 
     React.useEffect(() => {
@@ -35,6 +51,16 @@ export default function SignUpPage(props) {
                     maxHeight: "50%",
                 }}
             >
+            <TextField Labal="이름" onChange={(e) =>  setInputName(e.target.value)} placeholder="이름을 입력하세요"/>
+            <br/>
+            <TextField Labal="ID" onChange={(e) =>  setInputUserID(e.target.value)} placeholder="아이디를 입력하세요"/>
+            <br/>
+            <TextField Labal="Password" onChange={(e) =>  setInputUserPassword(e.target.value)} placeholder="패스워드를 입력하세요"/>
+            <br/>
+            <Button onClick={() => OnClickPostSignUp()}>
+            Sign
+            </Button>
+
             </Box>
         </div>
     );
